e90b37339d64122fca873c4518441a551cf34a3e
Hany Elemary <hany.elemary@gmail.com>
2017-01-22 22:44:59 -0500

    Fixing a broken app because of 'document' was not being defined in the server bundle.

--- src/api/search.js
+++ src/api/search.js
@@ -1,10 +1,9 @@
 import 'babel-polyfill';
 import fetch from 'isomorphic-fetch';
-import config from '../config';
 
 const search = (query) => {
   const encodedQuery = encodeURIComponent(query);
-  return fetch(`${config.BASE_URL}/api/search/${encodedQuery}`)
+  return fetch(`${document.location.origin}/api/search/${encodedQuery}`)
     .then(res => {
       return res.json();
     })
